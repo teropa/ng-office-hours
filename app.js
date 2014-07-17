0dd9449d661b941ea8ebf3f8d0ef8764342bbfa6
Tero Parviainen <tero@teropa.info>
2014-07-17 16:32:18 +0300

    use directive template to replace original content

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var app = angular.module('myApp', []);
 app.directive('expandableSection', function() {
   return {
     restrict: 'E',
+    template: '<section><h2>Title</h2><article>Body</article></section>',
+    replace: true,
     link: function($scope, $element) {
       $element.find('article').hide();
       $element.find('h2').click(function() {

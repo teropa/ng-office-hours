58ba386b73de95a25525e68abafcc99f0441c9b4
Tero Parviainen <tero@teropa.info>
2014-07-17 16:31:16 +0300

    switch to element directive

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 var app = angular.module('myApp', []);
 
 app.directive('expandableSection', function() {
-  return function($scope, $element) {
-    $element.find('article').hide();
-    $element.find('h2').click(function() {
-      $element.find('article').toggle();
-    });
+  return {
+    restrict: 'E',
+    link: function($scope, $element) {
+      $element.find('article').hide();
+      $element.find('h2').click(function() {
+        $element.find('article').toggle();
+      });
+    }
   };
 });

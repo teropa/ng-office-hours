67bfa31dcc084b903f2530fdf7428420a5438147
Tero Parviainen <tero@teropa.info>
2014-07-17 16:34:04 +0300

    restore dynamic content by getting it through attributes

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ var app = angular.module('myApp', []);
 app.directive('expandableSection', function() {
   return {
     restrict: 'E',
-    template: '<section><h2>Title</h2><article>Body</article></section>',
+    template: '<section><h2>{{title}}</h2><article>{{body}}</article></section>',
     replace: true,
-    link: function($scope, $element) {
+    link: function($scope, $element, $attrs) {
+      $scope.title = $attrs.sectionTitle;
+      $scope.body = $attrs.sectionBody;
       $element.find('article').hide();
       $element.find('h2').click(function() {
         $element.find('article').toggle();
